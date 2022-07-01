ede5382593406cb57b5e412051271ffe287ba808
liemlylac <liemlylac@gmail.com>
2022-07-01 21:45:26 +0700

    Add microservices and organize source as monorepo

--- apps/queue/src/main.ts
+++ apps/queue/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { QueueModule } from './queue.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(QueueModule);
   await app.listen(3000);
 }
+
 bootstrap();
